60f9ed6e32bfd4185eb17211d6a6d3e6ebf09fd5
Chris Dearman <chris@mips.com>
2011-12-14 13:19:13 -0800

    libOpenSLES needs libcutils for logging
    Change-Id: Ifa5715183ca78deb810d635a0c0dc789d0ff68ec
    
    Signed-off-by: Chris Dearman <chris@mips.com>

--- wilhelm/src/Android.mk
+++ wilhelm/src/Android.mk
@@ -207,7 +207,9 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -x c++ -DLI_API= -fvisibility=hidden -UNDEBUG \
                 -DSL_API='__attribute__((visibility("default")))'
-LOCAL_SHARED_LIBRARIES := libwilhelm
+LOCAL_SHARED_LIBRARIES := \
+        libwilhelm \
+        libcutils
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
